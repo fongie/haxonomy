42cace2e70163de41bc2d4335eccfcb081382796
fongie <max@maxk.se>
2019-04-03 14:31:42 +0200

    more inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,21 @@ VALUES
 ('account security', 1),
 ('encryption', 1);
 
+INSERT INTO term(name, broader_term_id)
+VALUES
+('programming language', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'language')),
+('markup language', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'language')),
+('frameworks & libraries', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'language'));
+
+INSERT INTO term(name, broader_term_id)
+VALUES
+('source code resources', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'source code')),
+('source code vulnerabilities', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'source code'));
+
+INSERT INTO term(name, broader_term_id)
+VALUES
+('request', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'network & communication')),
+('protocol', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'network & communication')),
+('redirect', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'network & communication')),
+('dns', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'network & communication'));
+
