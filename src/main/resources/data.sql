aa97c72c39bf9b87da645364856b41615214cf4e
Ossian Fjordefalk <ocean@ossians-mbp-2.mynet>
2019-05-02 15:27:08 +0200

    foreign key

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -297,5 +297,3 @@ VALUES
        ('csrf token', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'token'), 1),
        ('access token', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'token'), 1),
        ('oauth token', (SELECT id FROM (SELECT id, name FROM term) AS a WHERE name = 'token'), 1);
-
-SET FOREIGN_KEY_CHECKS = 1;
